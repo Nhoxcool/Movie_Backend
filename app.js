ddf6e6c6194f999fb9ef298de59a6b5fa11a6d6a
Tú Trần <nhoxcool2002@gmail.com>
2024-03-08 15:30:33 +0700

    Update_Actor

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
-const express = require('express');
-const morgan = require('morgan');
-require('express-async-errors')
-const {errorHandler} = require('./middlewares/error')
-const cors = require('cors')
+const express = require("express");
+const morgan = require("morgan");
+require("express-async-errors");
+const { errorHandler } = require("./middlewares/error");
+const cors = require("cors");
 
 require("dotenv").config();
-require('./db');
+require("./db");
 
-const userRouter = require('./routes/user');
-const { hadleNotFound } = require('./utils/helper');
+const userRouter = require("./routes/user");
+const actorRouter = require("./routes/actor");
+const { hadleNotFound } = require("./utils/helper");
 
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use('/api/user', userRouter);
-app.use('/*',hadleNotFound);
-
+app.use("/api/user", userRouter);
+app.use("/api/actor", actorRouter);
+app.use("/*", hadleNotFound);
 
 app.use(errorHandler);
 
 app.listen(8000, () => {
-    console.log("The port is listening on port 8000");
-})
+  console.log("The port is listening on port 8000");
+});
---
+++ controllers/actor.js
@@ -0,0 +1,81 @@
+const { isValidObjectId } = require("mongoose");
+const Actor = require("../models/actor");
+const { sendError } = require("../utils/helper");
+var cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+  secure: true,
+});
+
+exports.createActor = async (req, res) => {
+  const { name, about, gender } = req.body;
+  const { file } = req;
+
+  const newActor = new Actor({ name, about, gender });
+
+  //Chỉ upload avatar khi có file
+  if (file) {
+    const { secure_url, public_id } = await cloudinary.uploader.upload(
+      file.path,
+      { gravity: "face", height: 500, width: 500, crop: "thumb" }
+    );
+
+    newActor.avatar = { url: secure_url, public_id };
+  }
+
+  await newActor.save();
+
+  res.status(201).json({
+    id: newActor.id,
+    name,
+    about,
+    gender,
+    avatar: newActor.avatar?.url,
+  });
+};
+
+exports.updateActor = async (req, res) => {
+  const { name, about, gender } = req.body;
+  const { file } = req;
+
+  const { actorId } = req.params;
+
+  if (!isValidObjectId(actorId)) return sendError(res, "Invalid request!");
+  const actor = await Actor.findById(actorId);
+  if (!actor) return sendError(res, "Invalid request, record not found!");
+
+  const public_id = actor.avatar?.public_id;
+
+  if (public_id && file) {
+    const { result } = await cloudinary.uploader.destroy(public_id);
+    if (result !== "ok") {
+      return sendError(res, "Could not remove image from cloud!");
+    }
+  }
+
+  if (file) {
+    const { secure_url, public_id } = await cloudinary.uploader.upload(
+      file.path,
+      { gravity: "face", height: 500, width: 500, crop: "thumb" }
+    );
+
+    actor.avatar = { url: secure_url, public_id };
+  }
+
+  actor.name = name;
+  actor.about = about;
+  actor.gender = gender;
+
+  await actor.save();
+
+  res.status(201).json({
+    id: actor._id,
+    name,
+    about,
+    gender,
+    avatar: actor.avatar?.url,
+  });
+};
